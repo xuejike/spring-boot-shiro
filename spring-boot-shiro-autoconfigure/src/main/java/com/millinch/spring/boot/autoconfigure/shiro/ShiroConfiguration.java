03346657957a2fada0cf8e0538ed398a1252f2fe
xuejike <xuejike2004@126.com>
2018-06-29 16:24:23 +0800

    修复缓存兼容性问题

--- spring-boot-shiro-autoconfigure/src/main/java/com/millinch/spring/boot/autoconfigure/shiro/ShiroConfiguration.java
+++ spring-boot-shiro-autoconfigure/src/main/java/com/millinch/spring/boot/autoconfigure/shiro/ShiroConfiguration.java
@@ -46,7 +46,7 @@ public class ShiroConfiguration {
      */
     @Bean(name = "shiroCacheManager")
     @ConditionalOnMissingBean(name = "shiroCacheManager")
-    @ConditionalOnMissingClass(value = {"org.apache.shiro.cache.ehcache.EhCacheManager","org.springframework.cache.CacheManager"})
+//    @ConditionalOnMissingClass(value = {"org.apache.shiro.cache.ehcache.EhCacheManager","org.springframework.cache.CacheManager"})
     public CacheManager cacheManager() {
         return new MemoryConstrainedCacheManager();
     }
